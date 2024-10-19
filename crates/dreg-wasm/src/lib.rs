7470e71a9db7df988f8c0d20e28c71e8fae4aa4c
rtthw <172864013+rtthw@users.noreply.github.com>
2024-10-19 07:27:51 -0400

    Setup canvas getter

--- crates/dreg-wasm/src/lib.rs
+++ crates/dreg-wasm/src/lib.rs
@@ -1,1 +1,33 @@
 //! Web Assembly Platform for Dreg
+
+
+
+use dreg_core::prelude::*;
+use wasm_bindgen::JsCast as _;
+
+
+
+pub struct WasmPlatform {
+
+}
+
+impl Platform for WasmPlatform {
+    fn run(self, program: impl Program) -> Result<()> {
+        let window = web_sys::window()
+            .ok_or(anyhow::anyhow!("no global window exists"))?;
+        let document = window.document()
+            .ok_or(anyhow::anyhow!("should have a document on window"))?;
+        let body = document.body()
+            .ok_or(anyhow::anyhow!("document should have a body"))?;
+        let canvas = document.get_element_by_id("canvas")
+            .ok_or(anyhow::anyhow!("document should have a canvas"))?
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .map_err(|_| anyhow::anyhow!("canvas ID should correspond to a canvas element"))?;
+
+        while !program.should_exit() {
+
+        }
+
+        Ok(())
+    }
+}
