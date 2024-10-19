db1849363e94078ca7dea00355ead1813fd21f53
rtthw <172864013+rtthw@users.noreply.github.com>
2024-10-19 07:47:54 -0400

    Fix wasm feature gate

--- src/lib.rs
+++ src/lib.rs
@@ -8,4 +8,6 @@ pub mod prelude {
     pub use dreg_core::prelude::*;
     #[cfg(feature = "crossterm")]
     pub use dreg_crossterm::prelude::*;
+    #[cfg(feature = "wasm")]
+    pub use dreg_wasm::prelude::*;
 }
