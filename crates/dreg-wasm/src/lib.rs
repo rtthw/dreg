1f3b0b0eb8c3b46984ace0cd96a851e288e9d115
rtthw <172864013+rtthw@users.noreply.github.com>
2024-10-19 08:02:44 -0400

    Setup for rendering

--- crates/dreg-wasm/src/lib.rs
+++ crates/dreg-wasm/src/lib.rs
@@ -4,15 +4,19 @@
 
 use dreg_core::prelude::*;
 use wasm_bindgen::JsCast as _;
+use web_sys::{CanvasRenderingContext2d, Element};
 
 
 
 pub struct WasmPlatform {
-
+    context: Context,
+    buffers: [Buffer; 2],
+    current: usize,
+    last_known_size: Rect,
 }
 
 impl Platform for WasmPlatform {
-    fn run(self, program: impl Program) -> Result<()> {
+    fn run(mut self, mut program: impl Program) -> Result<()> {
         let window = web_sys::window()
             .ok_or(anyhow::anyhow!("no global window exists"))?;
         let document = window.document()
@@ -26,13 +30,47 @@ impl Platform for WasmPlatform {
         let canvas_ctx = canvas.get_context("2d")
             .map_err(|_| anyhow::anyhow!("canvas should support 2D rendering"))?
             .ok_or(anyhow::anyhow!("canvas 2D rendering context should exist"))?
-            .dyn_into::<web_sys::CanvasRenderingContext2d>()
+            .dyn_into::<CanvasRenderingContext2d>()
             .map_err(|_| anyhow::anyhow!("canvas 2D should be a rendering context"))?;
 
         while !program.should_exit() {
+            let size = self.size(&body);
+            self.autoresize(&canvas_ctx, size)?;
 
+            let frame = Frame {
+                context: &mut self.context,
+                area: size,
+                buffer: &mut self.buffers[self.current],
+            };
+
+            program.update(frame);
+            self.render();
+            self.swap_buffers();
         }
 
         Ok(())
+    }
+}
+
+impl WasmPlatform {
+    fn size(&self, body: &Element) -> Rect {
+        Rect::new(0, 0, body.client_width() as u16, body.client_height() as u16)
+    }
+
+    fn autoresize(&mut self, canvas_ctx: &CanvasRenderingContext2d, size: Rect) -> Result<()> {
+        if self.last_known_size != size {
+            let text_metrics = canvas_ctx.measure_text(" ")
+                .map_err(|_| anyhow::anyhow!("canvas context cannot measure text"))?;
+        }
+
+        Ok(())
+    }
+
+    fn render(&self) {}
+
+    /// Clear the inactive buffer and swap it with the current buffer.
+    fn swap_buffers(&mut self) {
+        self.buffers[1 - self.current].reset();
+        self.current = 1 - self.current;
     }
 }
