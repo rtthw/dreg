70d767dac2562129ac751cba239cd685ce06d40a
rtthw <172864013+rtthw@users.noreply.github.com>
2024-10-22 09:59:55 -0400

    Fix cell foreground styling on web

--- crates/dreg-wasm/src/lib.rs
+++ crates/dreg-wasm/src/lib.rs
@@ -227,25 +227,26 @@ impl Runner {
         let current_buffer = &self.buffers[self.current];
         let updates = previous_buffer.diff(current_buffer).into_iter();
 
-        // let text_color = self.program.on_platform_request("web::default_fill_style")
-        //     .unwrap_or("#bcbec4");
+        let text_color = self.program.on_platform_request("web::default_fill_style")
+            .unwrap_or("#bcbec4");
         // self.canvas_context.set_fill_style_str(text_color);
         self.canvas_context.set_text_align("left");
         self.canvas_context.set_text_baseline("top");
         for (x, y, cell) in updates {
             let (cell_w, cell_h) = (self.glyph_width, self.font_height);
             let (cell_x, cell_y) = (cell_w * x as f64, cell_h * y as f64);
-            let (real_x, real_y) = (cell_x - (cell_w * 0.5), cell_y - (cell_h * 0.5));
 
             self.canvas_context.clear_rect(cell_x, cell_y, cell_w, cell_h);
+
             // self.canvas_context.set_fill_style_str(&cell.bg.to_string().to_lowercase());
-            // self.canvas_context.fill_rect(
-            //     cell_x - (cell_w * 0.5),
-            //     cell_y - (cell_h * 0.5),
-            //     cell_w,
-            //     cell_h,
-            // );
-            self.canvas_context.set_fill_style_str(&cell.fg.to_string().to_lowercase());
+            // self.canvas_context.fill_rect(cell_x, cell_y, cell_w, cell_h);
+
+            let style = if cell.fg == Color::Reset {
+                text_color
+            } else {
+                &cell.fg.to_string().to_lowercase()
+            };
+            self.canvas_context.set_fill_style_str(style);
             let _r = self.canvas_context.fill_text(cell.symbol(), cell_x, cell_y);
         }
     }
