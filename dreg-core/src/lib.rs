4632beb1dad66dc27dbe3d97fce7ae2accd80397
rtthw <172864013+rtthw@users.noreply.github.com>
2025-01-30 14:53:25 -0500

    Reimplement some core functionality

--- dreg-core/src/lib.rs
+++ dreg-core/src/lib.rs
@@ -1,14 +1,44 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+//! Dreg Core
+
+
+
+use compact_str::CompactString;
+
+
+
+/// The object responsible for rendering [`Buffer`]s, handling user [`Input`], and responding to
+/// [`Platform`] requests,
+pub trait Program: 'static {
+    /// Update the program's state. This method is called every frame, regardless of user input.
+    fn update(&mut self, frame: Frame);
+
+    // /// This function is called whenever the running platform receives some user [`Input`].
+    // fn on_input(&mut self, input: Input);
+
+    /// This function is called every frame to determine whether the program should exit.
+    fn should_exit(&self) -> bool;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+
+pub struct Frame<'a> {
+    pub buffer: &'a mut Buffer,
+}
+
+impl<'a> Frame<'a> {
+    pub fn render(&mut self, text: Text) {
+        self.buffer.content.push(text);
     }
+}
+
+
+
+pub struct Buffer {
+    content: Vec<Text>,
+}
+
+pub struct Text {
+    content: CompactString,
+    x: u16,
+    y: u16,
 }
