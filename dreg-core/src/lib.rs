8a097b193a5d05a55b2f2c3b2db214565c806e8a
rtthw <172864013+rtthw@users.noreply.github.com>
2025-01-31 09:16:45 -0500

    Flesh out more text creation functionality

--- dreg-core/src/lib.rs
+++ dreg-core/src/lib.rs
@@ -47,6 +47,68 @@ pub struct Buffer {
 
 pub struct Text {
     content: CompactString,
-    x: u16,
-    y: u16,
+    pub x: u16,
+    pub y: u16,
+    /// The foreground color for the text.
+    pub fg: Color,
+
+    /// The background color for the text.
+    pub bg: Color,
+
+    /// The modifier for the text.
+    pub modifier: TextModifier,
+}
+
+impl Default for Text {
+    fn default() -> Self {
+        Self::EMPTY
+    }
+}
+
+impl Text {
+    /// An empty piece of text.
+    pub const EMPTY: Self = Self::new(" ");
+
+    /// Create a new piece of text with the given content.
+    ///
+    /// This works at compile time and puts the content onto the stack. It will fail to build when
+    /// the content doesn't fit onto the stack and needs to be placed on the heap.
+    ///
+    /// Use `Self::default().set_content()` in that case. See [`CompactString::const_new`] for more
+    /// details.
+    pub const fn new(content: &'static str) -> Self {
+        Self {
+            content: CompactString::const_new(content),
+            x: 0,
+            y: 0,
+            fg: Color::Reset,
+            bg: Color::Reset,
+            modifier: TextModifier::empty(),
+        }
+    }
+
+    /// Set the text's content.
+    pub fn with_content(&mut self, content: &str) -> &mut Self {
+        self.content = CompactString::new(content);
+        self
+    }
+
+    /// Set the text's position.
+    pub fn with_position(&mut self, x: u16, y: u16) -> &mut Self {
+        self.x = x;
+        self.y = y;
+        self
+    }
+
+    /// Set the text's x position.
+    pub fn with_x(&mut self, x: u16) -> &mut Self {
+        self.x = x;
+        self
+    }
+
+    /// Set the text's x position.
+    pub fn with_y(&mut self, y: u16) -> &mut Self {
+        self.y = y;
+        self
+    }
 }
