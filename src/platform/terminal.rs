fd2c8c062e8299a7a3ff2a9501d6e2d5af9b542c
rtthw <172864013+rtthw@users.noreply.github.com>
2025-02-05 07:02:16 -0500

    Reimplement some more terminal functionality

--- src/platform/terminal.rs
+++ src/platform/terminal.rs
@@ -4,6 +4,8 @@
 
 
 
+use crossterm::ExecutableCommand as _;
+
 use crate::Program;
 
 
@@ -13,6 +15,46 @@ pub struct TerminalPlatform;
 
 impl super::Platform for TerminalPlatform {
     fn run(self, program: impl Program) -> Result<(), Box<dyn std::error::Error>> {
+        bind_terminal()?;
+
+        // TODO: Main loop.
+
+        release_terminal()?;
+
         Ok(())
     }
+}
+
+
+
+fn bind_terminal() -> Result<(), Box<dyn std::error::Error>> {
+    let mut writer = std::io::stdout();
+    crossterm::terminal::enable_raw_mode()?;
+    writer.execute(crossterm::event::EnableMouseCapture)?;
+    writer.execute(crossterm::event::EnableFocusChange)?;
+    writer.execute(crossterm::terminal::EnterAlternateScreen)?;
+    writer.execute(crossterm::event::PushKeyboardEnhancementFlags(
+        crossterm::event::KeyboardEnhancementFlags::REPORT_EVENT_TYPES
+        | crossterm::event::KeyboardEnhancementFlags::REPORT_ALL_KEYS_AS_ESCAPE_CODES
+    ))?;
+    writer.execute(crossterm::cursor::Hide)?;
+    let original_hook = std::panic::take_hook();
+    std::panic::set_hook(Box::new(move |panic| {
+        release_terminal().unwrap();
+        original_hook(panic);
+    }));
+
+    Ok(())
+}
+
+fn release_terminal() -> Result<(), Box<dyn std::error::Error>> {
+    let mut writer = std::io::stdout();
+    crossterm::terminal::disable_raw_mode()?;
+    writer.execute(crossterm::event::DisableMouseCapture)?;
+    writer.execute(crossterm::event::DisableFocusChange)?;
+    writer.execute(crossterm::terminal::LeaveAlternateScreen)?;
+    writer.execute(crossterm::event::PopKeyboardEnhancementFlags)?;
+    writer.execute(crossterm::cursor::Show)?;
+
+    Ok(())
 }
