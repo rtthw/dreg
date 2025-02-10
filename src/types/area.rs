532a90747def3b3e682cdcd4b8795d038173b986
rtthw <172864013+rtthw@users.noreply.github.com>
2025-02-10 07:24:24 -0500

    Setup basic area splitting

--- src/types/area.rs
+++ src/types/area.rs
@@ -47,3 +47,60 @@ impl Area {
         input_context.middle_clicked(self)
     }
 }
+
+impl Area {
+    /// Horizontally split this area at the given length.
+    pub fn hsplit_len(&self, len: u16) -> (Self, Self) {
+        (
+            Self { x: self.x, y: self.y, w: len, h: self.h },
+            Self { x: len, y: self.y, w: self.w.saturating_sub(len), h: self.h },
+        )
+    }
+
+    /// Horizontally split this area at the given portion (from 0.0 to 1.0).
+    pub fn hsplit_portion(&self, portion: f32) -> (Self, Self) {
+        let len = (self.w as f32 * portion) as u16;
+        self.hsplit_len(len)
+    }
+
+    /// Vertically split this area at the given length.
+    pub fn vsplit_len(&self, len: u16) -> (Self, Self) {
+        (
+            Self { x: self.x, y: self.y, w: self.w, h: len },
+            Self { x: self.x, y: len, w: self.w, h: self.h.saturating_sub(len) },
+        )
+    }
+
+    /// Vertically split this area at the given portion (from 0.0 to 1.0).
+    pub fn vsplit_portion(&self, portion: f32) -> (Self, Self) {
+        let len = (self.w as f32 * portion) as u16;
+        self.hsplit_len(len)
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn splitting_works() {
+        let main_area = Area { x: 0, y: 0, w: 5, h: 7 };
+
+        let (left_area, right_area) = main_area.hsplit_len(3);
+
+        assert_eq!(left_area, Area { x: 0, y: 0, w: 3, h: 7 });
+        assert_eq!(right_area, Area { x: 3, y: 0, w: 2, h: 7 });
+
+        let (top_area, bottom_area) = main_area.vsplit_len(3);
+
+        assert_eq!(top_area, Area { x: 0, y: 0, w: 5, h: 3 });
+        assert_eq!(bottom_area, Area { x: 0, y: 3, w: 5, h: 4 });
+
+        let (left_area, right_area) = main_area.hsplit_portion(0.6);
+
+        assert_eq!(left_area, Area { x: 0, y: 0, w: 3, h: 7 });
+        assert_eq!(right_area, Area { x: 3, y: 0, w: 2, h: 7 });
+    }
+}
